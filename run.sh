285d5953f9fdfb103263c0381e773a9d3cb0660e
Jonas <jonas.luft@hcu-hamburg.de>
2019-11-25 16:33:30 +0100

    comments in run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 docker stop gracio_pygrakpi_instance
 docker rm gracio_pygrakpi_instance
-if [ "$#" -gt 0 ]; then
+if [ "$#" -gt 0 ]; then # if command line arguments were given
     docker run --name gracio_pygrakpi_instance -d gracio_pygrakpi --endpoint $1
-else
+else # no command line args -> don't choose endpoint
     docker run --name gracio_pygrakpi_instance -d gracio_pygrakpi
 fi
 docker logs -f gracio_pygrakpi_instance
